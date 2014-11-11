05f3ef8fc64484527c37490edbfaaceaa497edaa
etdeschenes <etienne.deschenes.1@ulaval.ca>
2014-11-10 22:54:21 -0500

    update ex08

--- bin/ex08.dart
+++ bin/ex08.dart
@@ -3,82 +3,128 @@ import 'package:ex08/fonctions.dart';
 //no. 1
 no1() {
   List associations = [
-    {"name": "VC", "description": "Volleyball club"}, 
+    {"name": "VC", "description": "Volleyball club"},
     {"name": "BC", "description": "Basketball club"}
   ];
   List members = [
-    {"associationCode" : "VC", 
-      "firstName": "Alfred", 
-      "lastName": "Clummed", 
+    {"associationCode" : "VC",
+      "firstName": "Alfred",
+      "lastName": "Clummed",
       "email": "ac@gmail.com"
     },
-    {"associationCode" : "VC", 
-      "firstName": "Henri", 
-      "lastName": "Mariacha", 
+    {"associationCode" : "VC",
+      "firstName": "Henri",
+      "lastName": "Mariacha",
       "email": "hm@gmail.com"
     },
-    {"associationCode" : "VC", 
-      "firstName": "Bertrand", 
-      "lastName": "Tillent", 
+    {"associationCode" : "VC",
+      "firstName": "Bertrand",
+      "lastName": "Tillent",
       "email": "bt@gmail.com"
     },
-    {"associationCode" : "VC", 
-      "firstName": "Robert", 
-      "lastName": "Verse", 
+    {"associationCode" : "VC",
+      "firstName": "Robert",
+      "lastName": "Verse",
       "email": "rv@gmail.com"
     },
-    {"associationCode" : "BC", 
-      "firstName": "Camille", 
-      "lastName": "Chenville", 
+    {"associationCode" : "BC",
+      "firstName": "Camille",
+      "lastName": "Chenville",
       "email": "cc@gmail.com"
     },
-    {"associationCode" : "BC", 
-      "firstName": "Catherine", 
-      "lastName": "Napind", 
+    {"associationCode" : "BC",
+      "firstName": "Catherine",
+      "lastName": "Napind",
       "email": "cn@gmail.com"
     },
-    {"associationCode" : "BC", 
-      "firstName": "Amelie", 
-      "lastName": "Bolders", 
+    {"associationCode" : "BC",
+      "firstName": "Amelie",
+      "lastName": "Bolders",
       "email": "ab@gmail.com"
     },
-    {"associationCode" : "BC", 
-      "firstName": "Zoe", 
-      "lastName": "Noyer", 
+    {"associationCode" : "BC",
+      "firstName": "Zoe",
+      "lastName": "Noyer",
       "email": "zf@gmail.com"
     }
   ];
 
+  print('====================================================================================');
   print('#1 : Classement de la liste des membres par nom de famille, prénom et lettre donnée.');
-  print("L'ordre en fonction du nom de famille : ${sortByLastName(members)}");
-  print("L'ordre en fonction du prénom : ${sortByFirstName(members)}");
-  print("Le nom de famille débutant par la lettre T : ${sortByLetter(members, 'T')}");
+  print('====================================================================================');
+  print("Association donnée à classer : $members");
+  print("L'ordre en fonction du nom de famille : ${extractByLastName(members)}");
+  print("L'ordre en fonction du prénom : ${extractByFirstName(members)}");
+  print("Le nom de famille débutant par la lettre T : ${extractByLetter(members, 'T')}");
   print('');
 }
 
 //no. 2
 no2() {
-  List vc = new List();
+  var associationTotal = new Association("","","");
+  var memberTotal = new Member("","","","");
+
   var association1 = new Association("VC", "Volleyball club", "To play volleyball");
+  associationTotal.addAssociation(association1);
+
   var member1 = new Member("VC", "Alfred", "Clummed", "ac@gmail.com");
+  memberTotal.addMember(member1);
+
   var member2 = new Member("VC", "Henri", "Mariacha", "hm@gmail.com");
-  vc.add(member1);
-  vc.add(member2);
-  List bc = new List();
+  memberTotal.addMember(member2);
+
+  var membership1 = new Membership(association1, member1, "reserve gym");
+  association1.memberships.add(membership1);
+  member1.memberships.add(membership1);
+
+  var membership2 = new Membership(association1, member2, "bring balls");
+  association1.memberships.add(membership2);
+  member2.memberships.add(membership2);
+
   var association2 = new Association("BC", "Basketball club", "To play basketball");
-  var member4 = new Member("BC", "Camille", "Chenville", "cc@gmail.com");
-  var member5 = new Member("BC", "Catherine", "Napind", "cn@gmail.com");
-  bc.add(member4);
-  bc.add(member5);
-  var member6 = new Member("VC", "Sylvain", "Garnier", "sg@gmail.com");
-  vc.add(member6);
-  association1.addMember(member6);
-  association2.deleteMember('ab@gmail.com');
-  association2.editMember("BC", "Catherine", "Napind", "cn@gmail.com");
+  associationTotal.addAssociation(association2);
+
+  var member3 = new Member("BC", "Camille", "Chenville", "cc@gmail.com");
+  memberTotal.addMember(member3);
+
+  var member4 = new Member("BC", "Catherine", "Napind", "cn@gmail.com");
+  memberTotal.addMember(member4);
+
+  var membership3 = new Membership(association2, member3, "reserve gym");
+  association2.memberships.add(membership3);
+  member3.memberships.add(membership3);
+
+  var membership4 = new Membership(association2, member4, "bring balls");
+  association2.memberships.add(membership4);
+  member4.memberships.add(membership4);
+
+  membership3.member.deleteMember('cc@gmail.com');
+  membership4.member.editMember("BC", "Catherine", "Napind", "cn@gmail.com");
+
+  print('===================================================================================');
   print("#2 : Classe permettant d'ajouter, d'enlever ou de modifier un membre d'association.");
-  print('$association1 $vc');
-  print('');
-  print('$association2 $bc');
+  print('===================================================================================');
+  for (var association in associationTotal) {
+    print('Association');
+    print('-----------');
+    print(association.toString());
+    print('    Members');
+    print('    -------');
+    for (var membership in association.memberships) {
+      print(membership.toString());
+    }
+  }
+  print('===============');
+  for (var members in memberTotal) {
+    print('Member');
+    print('------');
+    print(members.toString());
+    print('    Associations');
+    print('    ------------');
+    for (var membership in members.memberships) {
+      print(membership.toString());
+    }
+  }
 }
 
 main() {
--- lib/fonctions.dart
+++ lib/fonctions.dart
@@ -3,21 +3,21 @@ library ex08;
 //no. 1
 //Source : https://api.dartlang.org/apidocs/channels/stable/dartdoc-viewer/dart-core.List#id_sort
 
-List sortByLastName(List members) {
+List extractByLastName(List members) {
   List sorted = new List();
   members.sort((a, b) => a['lastName'].compareTo(b['lastName']));
   sorted.add(members);
   return sorted;
 }
 
-List sortByFirstName(List members) {
+List extractByFirstName(List members) {
   List sorted = new List();
   members.sort((a, b) => a['firstName'].compareTo(b['firstName']));
   sorted.add(members);
   return sorted;
 }
 
-List sortByLetter(List members, String letter) {
+List extractByLetter(List members, String letter) {
   List sorted = new List();
   for (var lastName in members) {
    if (lastName['lastName'][0] == letter)
@@ -33,10 +33,10 @@ class Association {
   String associationCode;
   String name;
   String description;
-  var assoMember = new List();
+  var memberships = new List();
 
-  Association(String this.associationCode, 
-              String this.name, 
+  Association(String this.associationCode,
+              String this.name,
               String this.description);
 
   String toString() {
@@ -47,25 +47,17 @@ class Association {
            '}\n';
   }
 
-  addMember(Member member) {
-      assoMember.add(member);
+  var associations = new List();
+  Iterator get iterator => associations.iterator;
+
+  addAssociation(Association association) {
+     associations.add(association);
   }
 
-  deleteMember(String email) {
-    for (var i = 0; i < assoMember.length; i++) {
-      if (assoMember[i].email == email) {
-          assoMember.removeAt(i);
-      }
-    }
-  }
-
-  editMember(String associationCode, String firstName, String lastName, String email) {
-    for (var i = 0; i < assoMember.length; i++) {
-      if (assoMember[i]["firstName"] == firstName && assoMember[i]["lastName"] == lastName) {
-          assoMember[i]["associationCode"] = associationCode;
-          assoMember[i]["firstName"] = firstName;
-          assoMember[i]["lastName"] = lastName;
-          assoMember[i]["email"] = email;
+  deleteAssociation(Association association) {
+    for (var i = 0; i < associations.length; i++) {
+      if (associations[i].name == association.name) {
+          associations.removeAt(i);
       }
     }
   }
@@ -76,31 +68,74 @@ class Member {
   String firstName;
   String lastName;
   String email;
-  var memberList = new List();
+  var memberships = new List();
 
-  Member(String this.associationCode, 
-         String this.firstName, 
-         String this.lastName, 
+  Member(String this.associationCode,
+         String this.firstName,
+         String this.lastName,
          String this.email);
 
   String toString() {
     return '{\n' 'Member \n'
            '  associationCode : ${associationCode} \n'
-           '  firstName: ${firstName}\n'
-           '  lastName: ${lastName}\n'
-           '  email: ${email}\n'
+           '  firstName : ${firstName}\n'
+           '  lastName : ${lastName}\n'
+           '  email : ${email}\n'
            '}\n';
   }
 
-  addAssociation(Association association) {
-     memberList.add(association);
+  var members = new List();
+  Iterator get iterator => members.iterator;
+
+  addMember(Member member) {
+      members.add(member);
   }
 
-  deleteAssociation(Association association) {
-    for (var i = 0; i < memberList.length; i++) {
-      if (memberList[i].name == association.name) {
-          memberList.removeAt(i);
+  deleteMember(String email) {
+    for (var i = 0; i < members.length; i++) {
+      if (members[i].email == email) {
+          members.removeAt(i);
+      }
+    }
+  }
+
+  editMember(String associationCode, String firstName, String lastName, String email) {
+    for (var i = 0; i < members.length; i++) {
+      if (members[i]["firstName"] == firstName && members[i]["lastName"] == lastName) {
+          members[i]["associationCode"] = associationCode;
+          members[i]["firstName"] = firstName;
+          members[i]["lastName"] = lastName;
+          members[i]["email"] = email;
       }
     }
   }
 }
+
+class Membership {
+  Association association;
+  Member member;
+  String description;
+  List memberships = new List();
+
+  Membership(Association this.association,
+             Member this.member,
+             String this.description);
+
+  String toString() {
+    return '    {\n'
+           '      association.name : ${association.name}\n'
+           '      firstName : ${member.firstName}\n'
+           '      lastName : ${member.lastName}\n'
+           '      description : ${description}\n'
+           '    }\n';
+  }
+
+  addMembership(Membership membership) {
+    memberships.add(membership);
+  }
+
+  deleteMembership(Membership membership) {
+    memberships.remove(membership);
+  }
+}
+
